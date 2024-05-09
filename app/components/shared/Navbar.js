2133b0312601d23d23a113b1bfcdd771c4806b05
masud040 <masud040@gmail.com>
2024-05-09 23:29:15 +0600

    initial setup done for this project

---
+++ app/components/shared/Navbar.js
@@ -0,0 +1,37 @@
+import Logo from "@/public/assets/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="container flex justify-between py-6">
+        <Link href="/">
+          <Image
+            src={Logo}
+            alt="Learn with sumit"
+            className="object-cover h-[54px]"
+          />
+        </Link>
+
+        <ul className="flex gap-4 text-sm text-gray-500">
+          <li className="py-2 active">
+            <Link href="./">Home</Link>
+          </li>
+
+          <li className="py-2">
+            <Link href="/recipe">Recipe</Link>
+          </li>
+
+          <li className="py-2">
+            <Link href="/about-us">About us</Link>
+          </li>
+
+          <li className="py-2 bg-[#eb4a36] px-6 rounded-md text-white content-center">
+            <Link href="/login">Login</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
