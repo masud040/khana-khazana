42ccfa30e20203b54c0b411577f1cb7ceac91dc8
masud040 <masud040@gmail.com>
2024-05-12 00:55:25 +0600

    add link coppy functionality

--- app/components/modal/SocailShareModal.js
+++ app/components/modal/SocailShareModal.js
@@ -9,16 +9,29 @@ import {
 } from "next-share";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function SocialShareModal({ recipeName }) {
   const pathname = usePathname();
+  const [copySuccess, setCopySuccess] = useState(false);
   const baseURL =
     typeof window !== "undefined" && window.location.origin
       ? window.location.origin
       : "";
-
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(`${baseURL}${pathname}`);
+      setCopySuccess(true);
+    } catch (error) {
+      console.log("Failed to copy:", error);
+    } finally {
+      setTimeout(() => {
+        setCopySuccess(false);
+      }, 3000);
+    }
+  };
   return (
-    <div className="absolute space-y-1 py-2 pt-0 h-52 bg-gray-100 rounded-md right-2 modal -top-[175px] text-balance backdrop-blur-3xl ">
+    <div className="absolute space-y-1 py-2 pt-0 h-52 bg-gray-100 rounded-md right-2 modal -top-[178px] text-balance backdrop-blur-3xl ">
       <div className="flex items-center justify-start px-2 py-1 rounded-t-md hover:bg-gray-200">
         <FacebookShareButton
           url={`${baseURL}${pathname}`}
@@ -52,9 +65,12 @@ export default function SocialShareModal({ recipeName }) {
         </PinterestShareButton>
       </div>
       <div className="flex items-center justify-start px-2 py-1 hover:bg-gray-200">
-        <button className="flex items-center gap-1 text-sm">
+        <button
+          onClick={copyToClipboard}
+          className="flex items-center gap-1 text-sm"
+        >
           <Image src={linkSvg} alt="copy" className="" />
-          <p>Copy link</p>
+          <p>{copySuccess ? "Copied" : "Copy link"}</p>
         </button>
       </div>
     </div>
